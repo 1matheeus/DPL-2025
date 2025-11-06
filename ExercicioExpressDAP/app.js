db21037ed34930a36117d6028e8bd994005f5309
1matheeus <borgess.matheus0@gmail.com>
2025-11-06 14:34:40 -0300

    Atualizar app.js do ExercicioExpressDAP

--- ExercicioExpressDAP/app.js
+++ ExercicioExpressDAP/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');    // ← ADICIONAR ESTE
+const fs = require('fs');        // ← ADICIONAR ESTE
 const app = express();
 app.use(express.json());
 
